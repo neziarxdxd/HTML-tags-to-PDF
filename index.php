94114166b06efd828e48166690c394fb71a8b9e3
zenraiching <rickdavidsangalang@gmail.com>
2021-03-27 12:10:38 +0800

    add

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'dompdf/autoload.inc.php'; 
+ 
+// Reference the Dompdf namespace 
+use Dompdf\Dompdf; 
+ 
+// Instantiate and use the dompdf class 
+$dompdf = new Dompdf();
+
+?>
