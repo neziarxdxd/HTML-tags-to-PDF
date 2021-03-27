e6bbd13ed2b36819e2c20cd2a43409e2d8acce03
zenraiching <rickdavidsangalang@gmail.com>
2021-03-27 12:12:37 +0800

    text

--- index.php
+++ index.php
@@ -6,5 +6,15 @@ use Dompdf\Dompdf;
  
 // Instantiate and use the dompdf class 
 $dompdf = new Dompdf();
+$dompdf->loadHtml('<h1>TEST</h1>'); 
+ 
+// (Optional) Setup the paper size and orientation 
+$dompdf->setPaper('A4', 'landscape'); 
+ 
+// Render the HTML as PDF 
+$dompdf->render(); 
+ 
+// Output the generated PDF to Browser 
+$dompdf->stream();
 
 ?>
